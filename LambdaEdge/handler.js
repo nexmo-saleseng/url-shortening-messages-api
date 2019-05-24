6541a1277e52574dfedc7260826391adcc0804a5
Enrico Portolan <eportolan@VONFVFYJ0ELHV2G.local>
2019-05-24 16:33:08 +0100

    - Fixed Lambda Edge function

--- LambdaEdge/handler.js
+++ LambdaEdge/handler.js
@@ -1,51 +1,81 @@
-const AWS = require( 'aws-sdk' );
-AWS.config.update( { region: 'eu-west-2' } );
-AWS.config.setPromisesDependency( require( 'bluebird' ) );
+const AWS = require('aws-sdk');
+
+AWS.config.update({ region: 'eu-west-2' });
+AWS.config.setPromisesDependency(require('bluebird'));
+const config = require('../config.json');
+
 const dynamoDb = new AWS.DynamoDB.DocumentClient();
-var rp = require('request-promise');
+const rp = require('request-promise');
 
-async function setReadUrl ( originUrl ) {
-    console.log( 'setReadUrl', originUrl )
-    const params = {
-        TableName: 'ShortenerUrl',
-        Key: {
-            "id": originUrl
-        },
-        UpdateExpression: "set clickedDate = :x, messageStatus= :messagestatus",
-
-        ExpressionAttributeValues: {
-            ":x": new Date().toISOString(),
-            ":messagestatus": 'clicked'
-        }
+async function sendWebhook(item) {
+  console.log('[sendWebhook]', item);
+  if (item && item.webhookClick) {
+    const options = {
+      method: 'POST',
+      uri: item.webhookClick,
+      timeout: 1500,
+      body: {
+        message_uuid: item.messageId,
+        status: item.messageStatus,
+        clicked_date: item.clickedDate,
+        creation_date: item.creationDate,
+        delivered_date: item.deliveredDate,
+        original_url: item.originalUrl,
+        short_url: item.id,
+      },
+      json: true, // Automatically stringifies the body to JSON
     };
-    try {
-        const updatedItem = await dynamoDb.update( params ).promise();
-        console.log( 'updatedItem', updatedItem );
-        const getItem = await dynamoDb.get( {
-            TableName: 'ShortenerUrl',
-            Key: {
-                "id": originUrl
-            }
-        } ).promise()
-        return getItem.Item;
-    } catch ( e ) {
-        console.log( 'e', e );
-        return null
-    }
-
+    console.log('[sendWebhook] - Send', options);
+    return rp(options).then((parsedBody) => {
+      console.log('rpThen', parsedBody);
+      return null;
+    })
+      .catch((err) => {
+        console.log('rpCatch', err);
+        return null;
+      });
+  }
 }
 
-exports.handler = async ( event ) => {
+async function setReadUrl(originUrl) {
+  console.log('setReadUrl', originUrl);
+  const params = {
+    TableName: config.DYNAMODB_TABLE,
+    Key: {
+      id: originUrl,
+    },
+    UpdateExpression: 'set clickedDate = :x, messageStatus= :messagestatus',
 
-    const { request, origin } = event.Records[ 0 ].cf;
-    console.log( 'request', request )
-    console.log( 'headers', request.headers )
-    console.log( 'origin', origin )
-    try {
-        await setReadUrl( `https://a.nxdm.be${request.uri}` )
-    } catch ( e ) {
-        console.log( 'Err:', e )
-    }
+    ExpressionAttributeValues: {
+      ':x': new Date().toISOString(),
+      ':messagestatus': 'clicked',
+    },
+  };
+  try {
+    const updatedItem = await dynamoDb.update(params).promise();
+    console.log('updatedItem', updatedItem);
+    const getItem = await dynamoDb.get({
+      TableName: config.DYNAMODB_TABLE,
+      Key: {
+        id: originUrl,
+      },
+    }).promise();
+    await sendWebhook(getItem.Item);
+    return null;
+  } catch (e) {
+    console.log('e', e);
+    return null;
+  }
+}
 
-    return request;
+module.exports.hookLambdaClick = async (event) => {
+  const { request } = event.Records[0].cf;
+  console.log('request', request);
+  try {
+    await setReadUrl(`${config.BASE_URL}${request.uri}`);
+  } catch (e) {
+    console.log('Err:', e);
+  }
+
+  return request;
 };
